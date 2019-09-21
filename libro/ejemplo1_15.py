392f9d4df757f8acbdaf0259f8c1bd9386ff701c
AxlDixon <axl.dxn@gmail.com>
2019-09-20 21:46:14 -0500

    Problema 1_15 del libro

---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,5 @@
+BASE = float(input("Escribe la base del rectangulo:"))
+ALTU = float(input("Escribe la altura del rectangulo:"))
+SUP = BASE*ALTU
+PER = 2 * (BASE + ALTU)
+print("La superficie del rectangulo dado que la base es {BASE} y la altura es {ALTU} es igual a {SUP}, mientras que el perimetro del rectangulo dado que la base es {BASE} y la altura es {ALTU} es igual a {PER}.")
