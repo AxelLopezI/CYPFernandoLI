f058d0b9a36dc806b53b35a9c8e6cd8bb72963b3
AxlDixon <axl.dxn@gmail.com>
2019-09-22 23:22:31 -0500

    Actualización del Problema 1_4

--- libro/Problemas_resueltos/capitulo1/problema1_4.py
+++ libro/Problemas_resueltos/capitulo1/problema1_4.py
@@ -1,1 +1,4 @@
-GAL = float(input("))
+GAL = float(input("Introduzca el nùmero de galones que han sido suministrados:"))
+LIT = GAL*3.785
+TOTAL = GAL*3.785*8.20
+print(f"La cantidad de dinero que hay que cobrar al cliente, dado que se suministraron {GAL} galones que corresponden a {LIT} litros y el precio de cada litro es 8.20, es ${TOTAL}.")
