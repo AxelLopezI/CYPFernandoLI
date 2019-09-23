169a50a332ef466ec3412ebfb5aff07ba57e2367
AxlDixon <axl.dxn@gmail.com>
2019-09-23 00:01:18 -0500

    Problema 1_8 del libro

---
+++ libro/Problemas_resueltos/capitulo1/problema1_8.py
@@ -0,0 +1,6 @@
+X1 = float(input("Introduzca el valor de X1:"))
+Y1 = float(input("Introduzca el valor de Y1:"))
+X2 = float(input("Introduzca el valor de X2:"))
+Y2 = float(input("Introduzca el valor de Y2:"))
+DIS = ((X1-X2)**2 + (Y1-Y2)**2)**0.5
+print("La distancia entre los puntos P1({X1},{Y1}) y P2({X2},{Y2}) es de {DIS} unidades.")
