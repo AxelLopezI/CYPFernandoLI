aa204ab891ff759359dc0b9ac0c440980cf00b68
AxlDixon <axl.dxn@gmail.com>
2019-09-23 23:43:35 -0500

    Actualización del Problema 1_7

--- libro/Problemas_resueltos/capitulo1/problema1_7.py
+++ libro/Problemas_resueltos/capitulo1/problema1_7.py
@@ -3,5 +3,4 @@ L2 = float(input("Introduzca la medida del lado 2 del triangulo:"))
 L3 = float(input("Introduzca la medida del lado 3 del triangulo:"))
 S = (L1+L2+L3)/2
 AREA = (S*(S-L1)*(S-L2)*(S-L3))**0.5
-print(f"El área del triangulo es:")
-
+print(f"El área del triangulo es {AREA}.")
