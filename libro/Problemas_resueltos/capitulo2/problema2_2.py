3f427d0cd30d29e00b3cd5cdc452f561cd45c96f
AxlDixon <axl.dxn@gmail.com>
2019-10-01 22:11:16 -0500

    Se agregó el problema 2_2 del libro

---
+++ libro/Problemas_resueltos/capitulo2/problema2_2.py
@@ -0,0 +1,6 @@
+P = int(input("Introduzca el valor entero de la primera variable:"))
+Q = int(input("Introduzca el valor entero de la segunda variable:"))
+EXP = P**3 + Q**4 - 2*P**2
+if EXP < 680:
+    print(f"Los valores {P} y {Q} satisfacen la expresión.")
+print("Fin del programa")
