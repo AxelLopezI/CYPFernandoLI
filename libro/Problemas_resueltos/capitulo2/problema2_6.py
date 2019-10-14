4afc41145c2ee5209dcf9e2361ece202b558a020
AxlDixon <axl.dxn@gmail.com>
2019-10-14 01:12:34 -0500

    Se agregó el problema 2_6 del libro

---
+++ libro/Problemas_resueltos/capitulo2/problema2_6.py
@@ -0,0 +1,9 @@
+A = int(input("Introduzca un número entero:"))
+if A == 0:
+    print("Nulo.")
+else:
+    if ((-1)**A) > 0:
+        print("Par.")
+    else:
+        print("Impar.")
+print("Fin del programa!!")
