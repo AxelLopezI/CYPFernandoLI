a621326d2014fd3a7d7a402efe79bcb92bac4811
AxlDixon <axl.dxn@gmail.com>
2019-10-14 01:07:21 -0500

    Se agregó el problema 2_5 del libro

---
+++ libro/Problemas_resueltos/capitulo2/problema2_5.py
@@ -0,0 +1,9 @@
+NUM = int(input("Introduzca un numero entero:"))
+if NUM > 0:
+    print("Positivo.")
+else:
+    if NUM == 0:
+        print("Nulo.")
+    else:
+        print("Negativo.")
+print("Fin del programa!!")
