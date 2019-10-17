9ff9af1fc1b0201d5fffce8c8f413e799521d5e3
AxlDixon <axl.dxn@gmail.com>
2019-10-16 22:33:46 -0500

    Se agregó el problema 2_11 del libro

--- libro/Problemas_resueltos/capitulo2/problema2_11.py
+++ libro/Problemas_resueltos/capitulo2/problema2_11.py
@@ -1,29 +1,17 @@
-A = float(input("Introduzca un número real:"))
-B = float(input("Introduzca un número real:"))
-C = float(input("Introduzca un número real:"))
-if A > B:
-    if A > C:
-        print(f"{A} el es mayor.")
-    else:
-        if A == C:
-            print(f"{A} y {C} son los mayores.")
-        else:
-            print(f"{C} es el mayor.")
-else:
-    if A == B:
-        if A > C:
-            print(f"{A} y {B} son los mayores.")
-        else:
-            if A == C:
-                print(f"{A}, {B} y {C} son iguales.")
-            else:
-                print(f"{C} es el mayor.")
-    else:
-        if B > C:
-            print(f"{B} es el mayor.")
-        else:
-            if B == C:
-                print(f"{B} y {C} son los mayores.")
-            else:
-                print(f"{C} es el mayor.")
-print("Fin del programa!!")
+CLAVE = int(input("Introduzca la clave de la zona geográfica a la que se llamó:"))
+NUMIN = int(input("Introduzca la duración (en minutos) de la llamada:"))
+if CLAVE == 12:
+    COST = NUMIN*2
+elif CLAVE == 15:
+    COST = NUMIN*2.2
+elif CLAVE == 18:
+    COST = NUMIN*4.5
+elif CLAVE == 19:
+    COST = NUMIN*3.5
+elif CLAVE == 23:
+    COST = NUMIN*6
+elif CLAVE == 25:
+    COST = NUMIN*6
+elif CLAVE == 29:
+    COST = NUMIN*5
+print("Costo total de la llamada", COST)
