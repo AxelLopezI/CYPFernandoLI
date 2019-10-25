60bb41a8f88be93a3abed307c52dff56384b2263
AxlDixon <axl.dxn@gmail.com>
2019-10-24 23:52:41 -0500

    Se agregó el problema 3_4 del libro

--- libro/Problemas_resueltos/capitulo3/problema3_4.py
+++ libro/Problemas_resueltos/capitulo3/problema3_4.py
@@ -1,12 +1,13 @@
 NOM = 0
 SUE = float(input("Introduzca el sueldo del trabajador:"))
-while (SUE % -1):
+while (SUE != -1):
     if SUE < 1000:
         NSUE = SUE * 1.15
     else:
         NSUE = SUE * 1.12
-    NOM += NSUE
+    NOM = NOM + NSUE
+    print(f"{NSUE}")
     SUE = bool(int(input("¿Hay otro sueldo? (SI=1, NO=0):")))
-print(f"El nuevo sueldo del trabajador es de {NNSUE}.")
+print(f"El nuevo sueldo del trabajador es de {NSUE}.")
 print(f"El acumulado de los nuevos sueldos de los trabajadores es de {NOM}.")
 print(f"Fin del programa!!")
